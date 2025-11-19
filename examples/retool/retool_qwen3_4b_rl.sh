cb300b322c4b37a239b7e1b857ec3064aeec5d3b
Li, Zhuohao <zhuohao.li@alibaba-inc.com>
2025-11-19 11:14:34 -0800

    dbg

--- examples/retool/retool_qwen3_4b_rl.sh
+++ examples/retool/retool_qwen3_4b_rl.sh
@@ -97,10 +97,10 @@ OPTIMIZER_ARGS=(
 )
 
 WANDB_ARGS=(
-   --use-wandb
-   --wandb-project miles-dapo
-   --wandb-group qwen3-4B-test-multi-turn
-   --wandb-key ${WANDB_KEY}
+   # --use-wandb
+   # --wandb-project miles-dapo
+   # --wandb-group qwen3-4B-test-multi-turn
+   # --wandb-key ${WANDB_KEY}
 )
 
 SGLANG_ARGS=(
